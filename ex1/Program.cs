16d320d55ecb0ab71ba5e8da382bf06a7d637983
Савенчук Любовь <sav_lu@gmail.com>
2022-09-18 23:52:16 +0700

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int [,] array = new int [4,4];
+Random rnd = new Random();
+for(int i = 0; i < array.GetLength(0); i++)
+{
+   
+   for(int j = 0; j < array.GetLength(1); j++)
+   {
+      array[i,j] = rnd.Next(10,20);
+      Console.Write(array[i,j] + " ");
+   }
+   Console.WriteLine();
+}
+int sum = 0;
+int sum1 = 0;
+int sum2 = 0;
+int row = 0;
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+   sum2 = 0;
+   for(int j = 0; j < array.GetLength(1); j++)
+   {
+      sum2 += array[i, j];
+   }  
+      if (sum2 < sum1)
+      {
+         sum = sum2;
+         row = i;
+      }
+      sum1 = sum2;
+}
+Console.WriteLine($"Наименьшая сумма равна {sum}");
+Console.WriteLine($"Строка с наименьшей суммой {row}");
